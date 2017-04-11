cf834cd0a079d20334515cc61ce3fdf71e456120
Joseph <Rudeseal>
2017-04-11 00:40:52 -0400

    Created Repo

---
+++ LedProjectWPF/AsusVgaAura.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AsusVgaAura
+{
+    public enum LightingMode : UInt32
+    {
+        None = 0x00,
+        Static = 0x01,
+        Breath = 0x02,
+        Strobe = 0x03,
+        CrossfadeColorSpectrum = 0x04
+    }
+    [StructLayout(LayoutKind.Explicit)]
+    public struct LedColor
+    {
+        [FieldOffset(0)]
+        public UInt32 Red;
+        [FieldOffset(4)]
+        public UInt32 Green;
+        [FieldOffset(8)]
+        public UInt32 Blue;
+    }
+    [StructLayout(LayoutKind.Explicit, Size = 64)]
+    public struct InteropLedInfo
+    {
+        [FieldOffset(0)]
+        public UInt32 Unknown1;
+
+        [FieldOffset(4)]
+        public UInt32 Unknown2;
+
+        [FieldOffset(8)]
+        public UInt32 Unknown3;
+
+        /// <summary>
+        /// Unsure, Is always 0x01
+        /// </summary>
+        [FieldOffset(12)]
+        public UInt32 Unknown4;
+
+        [FieldOffset(16)]
+        public LedColor Color;
+
+        /// <summary>
+        /// needs to be 0x01 in order to disable LEDs or change LightingMode
+        /// </summary>
+        [FieldOffset(28)]
+        public UInt32 ChangeState;
+
+        [FieldOffset(32)]
+        public LightingMode Mode;
+    };
+
+    public class AsusVgaAuraWrapper
+    {
+        [DllImport(dllName: "VGA_Extra.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void LoadVenderDLL();
+
+        [DllImport(dllName: "VGA_Extra.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Set_LED_ExternalFan_Info(InteropLedInfo ledInfo);
+
+        public AsusVgaAuraWrapper()
+        {
+            LoadVenderDLL();
+        }
+
+        public void SetColor(byte red, byte green, byte blue, LightingMode lightingMode = LightingMode.Static)
+        {
+            InteropLedInfo data = new InteropLedInfo();
+            data.Unknown4 = 0x01;
+            data.Color.Red = red;
+            data.Color.Green = green;
+            data.Color.Blue = blue;
+            data.ChangeState = 0x01;
+            data.Mode = lightingMode;
+            Set_LED_ExternalFan_Info(data);
+        }
+
+        //Sets the LEDs to fade across the color spectrum at an undefined rate
+        public void SetCrossFade()
+        {
+            InteropLedInfo data = new InteropLedInfo();
+            data.ChangeState = 0x01;
+            data.Mode = LightingMode.CrossfadeColorSpectrum;
+            Set_LED_ExternalFan_Info(data);
+        }
+
+        //Turns the LEDs off
+        public void DisableLights()
+        {
+            InteropLedInfo data = new InteropLedInfo();
+            data.Unknown4 = 0x01;
+            data.Color.Red = 0x00;
+            data.Color.Green = 0x00;
+            data.Color.Blue = 0x00;
+            data.ChangeState = 0x01;
+            data.Mode = LightingMode.Static;
+            Set_LED_ExternalFan_Info(data);
+        }
+
+        //Freezes the LEDs in their current state (brightness, rgb values, etc.)
+        public void FreezeLight()
+        {
+            InteropLedInfo data = new InteropLedInfo();
+            data.Unknown4 = 0x01;
+            data.Color.Red = 0x00;
+            data.Color.Green = 0x00;
+            data.Color.Blue = 0x00;
+            data.ChangeState = 0x01;
+            data.Mode = LightingMode.None;
+            Set_LED_ExternalFan_Info(data);
+        }
+    }
+}
---
+++ LedProjectWPF/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CUE.NET;
+using CUE.NET.Devices.Keyboard;
+using CUE.NET.Exceptions;
+using CUE.NET.Devices.Generic.Enums;
+
+namespace LedProjectWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AsusVgaAura.AsusVgaAuraWrapper graphicsCard = new AsusVgaAura.AsusVgaAuraWrapper();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                CueSDK.Initialize();
+                Debug.WriteLine("Initialized with " + CueSDK.LoadedArchitecture + "-SDK");
+
+                CorsairKeyboard keyboard = CueSDK.KeyboardSDK;
+                if (keyboard == null)
+                    throw new WrapperException("No Keyboard Found");
+            }
+            catch (CUEException ex)
+            {
+                Debug.WriteLine("CUE Exception! ErrorCode: " + Enum.GetName(typeof(CorsairError), ex.Error));
+            }
+            catch (WrapperException ex)
+            {
+                Debug.WriteLine("Wrapper Exception! Message: " + ex.Message);
+            }
+        }
+
+        private void LightsOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            graphicsCard.DisableLights();
+        }
+
+        private void FadeColorsButton_Click(object sender, RoutedEventArgs e)
+        {
+            graphicsCard.SetCrossFade();
+        }
+
+        private void SyncWithCorsairKeyboard_Click(object sender, RoutedEventArgs e)
+        {
+            graphicsCard.DisableLights();
+            CorsairKeyboard keyboard = CueSDK.KeyboardSDK;
+            Debug.WriteLine("Keyboard Found: " + CueSDK.IsSDKAvailable(CorsairDeviceType.Keyboard));
+            int r = keyboard['A'].Color.R;
+            graphicsCard.SetColor(r, g, b);
+        }
+    }
+}
